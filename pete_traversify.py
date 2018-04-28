ad291029fae46e1f3bf8e6d84d13785cd59ab9d1
mingtea <pitseleh79@gmail.com>
2018-04-28 09:38:38 -0400

    Minor changes to the regex
    Changed the regex to also search for 'triumphs' and 'triumphant'.

--- pete_traversify.py
+++ pete_traversify.py
@@ -13,7 +13,7 @@ url_root = "https://www.rollingstone.com/contributor/peter-travers?page="
 #counting how many instances of triumph we find
 triumph_counter = 0
 #the regex that matches triumph, don't care if it's upper or lower case
-triumph_regex = re.compile(r'triumph(ant)?', re.I)
+triumph_regex = re.compile(r'triumph(s)?(ant)?', re.I)
 
 #each page has like 15 reviews on it. this is the base page
 page_urls = []
@@ -43,10 +43,16 @@ for review in review_urls:
 	r = get(review)
 	soup = BeautifulSoup(r.content, 'html.parser')
 	para = soup.find_all('p')
+	header = soup.find_all('h1', class_="content-title")
 	for p in para:
 		text = p.text
 		for t in re.finditer(triumph_regex, text):
 			triumph_counter += 1
+	for h in header:
+		text = h.text
+		for t in re.finditer(triumph_regex, text):
+			triumph_counter += 1	
+
 print("I combed "+ str(len(review_urls))+ " reviews and found "+str(triumph_counter)+ " instances of the word triumph.")
 
 
